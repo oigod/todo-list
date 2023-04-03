2de72eb7efec9c09d6dba080191e65332538f6d1
oithegod <serejathegod@gmail.com>
2023-04-03 13:35:29 +0300

    Added urls for CRUD TaskModel views

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', views.ToDoListView.as_view(), name='todo-list'),
     path('tags-list/', views.TagsListView.as_view(), name='tags-list'),
     path('<int:pk>/switch-todo-status/', views.switch_todo_status, name='switch-todo-status'),
+    path('create-task/', views.ToDoTaskCreateView.as_view(), name='create-task'),
+    path('<int:pk>/update-task/', views.ToDoTaskUpdateView.as_view(), name='update-task'),
+    path('<int:pk>/delete-task/', views.ToDoTaskDeleteView.as_view(), name='delete-task'),
 ]
