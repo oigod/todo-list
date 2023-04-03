1a6dcd6a3e988b3f9c07692e638adc12a4a42eef
oithegod <serejathegod@gmail.com>
2023-04-03 11:51:10 +0300

    Register models to admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+@admin.register(models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("content",)
+    list_display = ("content", "datetime", "deadline", "is_done")
+    list_filter = ("is_done",)
