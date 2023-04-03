db37c9a8b02ffafdfedb1143da4aac2dffe503f9
oithegod <serejathegod@gmail.com>
2023-04-03 13:35:09 +0300

    Added CRUD functionality for TaskModel

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
-from . import models
+from . import models, forms
 
 
 class ToDoListView(generic.ListView):
@@ -9,6 +10,26 @@ class ToDoListView(generic.ListView):
     context_object_name = 'todo_list'
     template_name = 'todo_templates/todo_list.html'
     paginate_by = 5
+
+
+class ToDoTaskCreateView(generic.CreateView):
+    model = models.Task
+    form_class = forms.TaskForm
+    template_name = 'todo_templates/task_form.html'
+    success_url = reverse_lazy("todo:todo-list")
+
+
+class ToDoTaskUpdateView(generic.UpdateView):
+    model = models.Task
+    form_class = forms.TaskForm
+    template_name = 'todo_templates/task_form.html'
+    success_url = reverse_lazy("todo:todo-list")
+
+
+class ToDoTaskDeleteView(generic.DeleteView):
+    model = models.Task
+    template_name = 'todo_templates/task_confirm_delete.html'
+    success_url = reverse_lazy("todo:todo-list")
 
 
 class TagsListView(generic.ListView):
