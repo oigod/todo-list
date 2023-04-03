4948665d512f77c38608de2215c119ecc47afc0a
oithegod <serejathegod@gmail.com>
2023-04-03 13:50:05 +0300

    Added urls for CRUD functional for  Tag Model

--- todo/urls.py
+++ todo/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('tags-list/', views.TagsListView.as_view(), name='tags-list'),
     path('<int:pk>/switch-todo-status/', views.switch_todo_status, name='switch-todo-status'),
     path('create-task/', views.ToDoTaskCreateView.as_view(), name='create-task'),
+    path('create-tag/', views.TagsCreateView.as_view(), name='create-tag'),
     path('<int:pk>/update-task/', views.ToDoTaskUpdateView.as_view(), name='update-task'),
+    path('<int:pk>/update-tag/', views.TagUpdateView.as_view(), name='update-tag'),
     path('<int:pk>/delete-task/', views.ToDoTaskDeleteView.as_view(), name='delete-task'),
+    path('<int:pk>/delete-tag/', views.TagDeleteView.as_view(), name='delete-tag'),
 ]
